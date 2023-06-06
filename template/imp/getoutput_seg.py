6201d75aee0b294de44e2fa672fcb58595890edc
Steffen Backes <steffen@hemulen.ad12.riken.jp>
2023-06-06 13:45:14 +0900

    fixed getoutput_seg.py

--- template/imp/getoutput_seg.py
+++ template/imp/getoutput_seg.py
@@ -6,13 +6,20 @@ inf = open('Swl.dat','r')
 inf2 = open('../g_bath.dat','r')
 outf =open('Gwl.dat','w')
 
+ii = 0
 for line in inf:
 	dataS  = [float(x) for x in line.split()]
 	dataG0 = [float(x) for x in inf2.readline().split()]
 
 	w = dataS[0]
 	norb = int( (len(dataS)-1)/4 )
+	norbDmft = int( np.sqrt( (len(dataG0)-1)/4 ) )
 
+	if (ii==0):
+		print( "norbImp = ",norb )
+		print( "norbDMFT = ",norbDmft )
+	ii+=1
+   
 	outf.write( str( w ) + '\t')
 
 	for s in range(2):
@@ -21,7 +28,7 @@ for line in inf:
 		for m1 in range(norb):
 			sig[m1,m1] = dataS[1+m1*4+2*s+0] + dataS[1+m1*4+2*s+1]*1.0j
 			for m2 in range(norb):
-				g0[m1,m2] = dataG0[1+s*(norb**2)*2+m2*norb*2+m1*2+0] + dataG0[1+s*(norb**2)*2+m2*norb*2+m1*2+1]*1.0j
+				g0[m1,m2] = dataG0[1+s*(norbDmft**2)*2+m2*norbDmft*2+m1*2+0] + dataG0[1+s*(norbDmft**2)*2+m2*norbDmft*2+m1*2+1]*1.0j
 
 		g = np.linalg.inv( np.linalg.inv(g0) - sig )
 
